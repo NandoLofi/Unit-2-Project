ea728a86567b7088daaf62467f45557dd25d616c
Fernando Galvan <n.brighter00@gmail.com>
2022-04-28 16:33:36 -0500

    final product

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-const morgan = require('morgan')
 const path = require('path');
 const PORT = process.env.PORT
 
 
-//logger
-app.use(morgan('tiny'));
 
 
 //middleware
