2f71207b4601a25e407393da68965e4d9d5d182f
Fernando Galvan <n.brighter00@gmail.com>
2022-04-28 19:42:19 -0500

    fixed mongo connect

--- server.js
+++ server.js
@@ -23,16 +23,6 @@ app.use('/css', express.static(path.resolve(__dirname, 'public/css')))
 //router
 app.use('/', require('./controllers/routes'))
 
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,  
-});
-
-const db = mongoose.connection;
-db.on('error', (err) => console.log(err.message + " is mongo not running?"));
-db.on('connected', ()=> console.log('mongoose connected'));
-db.on('disconnected', () => console.log("mongo disconnected"));
-
-
+mongoose.connect(process.env.DATABASE_URL);
 
 app.listen(PORT, () => console.log('We are running'));
